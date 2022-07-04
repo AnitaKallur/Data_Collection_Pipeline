99b4d01b4efc6b4917b7fb13a717eaabfb69b32e
AnitaKallur <tarangi.anita@gmail.com>
2022-07-04 19:19:58 +0100

    indeed

--- soup.py
+++ soup.py
@@ -36,15 +36,18 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
 
-def extract(page):
+def extract():
     # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    s_url = driver.get("https://uk.indeed.com/jobs?q=Data%20Engineer%20or%20Data%20Scientist%20&l=London%2C%20Greater%20London&start={page}")
-    url = (f'https://uk.indeed.com/jobs?q=Data%20Engineer%20or%20Data%20Scientist%20&l=London%2C%20Greater%20London&start={page}')
-    response = requests.get(url)
-    soup = bs(response.content, 'html.parser')
-    cards = soup.find_all('div', 'jobsearch-SerpJobcard')
-    print(cards)
+    s_url = driver.get("https://uk.indeed.com/jobs?q=Data%20Engineer%20or%20Data%20Scientist%20&l=London%2C%20Greater%20London&vjk=f11971796d62ded9")
+    # url = (f'https://uk.indeed.com/jobs?q=Data%20Engineer%20or%20Data%20Scientist%20&l=London%2C%20Greater%20London&start={page}')
+    # response = requests.get(url)
+    # soup = bs(response.content, 'html.parser')
+    # cards = soup.find_all('div', 'jobsearch-SerpJobcard')
+    # pagination = driver.find_element(by=By.XPATH, value = "//a[@aria-label='2']")
+    # pagination.click()
+    
+    # print()
     # print(response.json)
     # print(response.text)
     
@@ -63,28 +66,30 @@ def extract(page):
     
     
     
-# def get_job_details(self, job_containers):
+def get_job_details():
     
-#     """ Scraping all the details related to the job post"""
-#     list_of_all_jobs_details = []
+    """ Scraping all the details related to the job post"""
+    list_of_all_jobs_details = []
     
-#     for job_listing in job_containers:
-#         global job_details_dictionary
-#         job_details_dictionary = dict()
-#         job_details_dictionary["Job_Link"] = job_listing.find_element(by=By.XPATH, value=".//a").get_attribute('href')
-#         job_details_dictionary["Unique_ID"] = job_listing.find_element(by=By.XPATH, value=".//a").get_attribute('id')
-#         job_details_dictionary["Title"] = job_listing.find_element(by=By.XPATH, value=".//h2").text
-#         job_details_dictionary["Company_Name"] = job_listing.find_element(by=By.XPATH, value=".//div/span[@class='companyName']").text
-#         job_details_dictionary["Company_Location"] = job_listing.find_element(by=By.XPATH, value=".//div[@class='companyLocation']").text
-#         try:
-#             job_details_dictionary["Salary"] = job_listing.find_element(by=By.XPATH, value= ".//div[@class='metadata salary-snippet-container']").get_attribute('textContent')
-#         except:
-#             pass   
-#         list_of_all_jobs_details.append(job_details_dictionary)
-#         print(list_of_all_jobs_details)
-#     # return list_of_all_jobs_details
-  
-extract(page='https://uk.indeed.com/jobs?q=Data%20Engineer%20or%20Data%20Scientist%20&l=London%2C%20Greater%20London&start=0')
+    for job_listing in list_of_all_jobs_details:
+        global job_details_dictionary
+        job_details_dictionary = dict()
+        job_details_dictionary["Job_Link"] = job_listing.find_element(by=By.XPATH, value=".//a").get_attribute('href')
+        job_details_dictionary["Unique_ID"] = job_listing.find_element(by=By.XPATH, value=".//a").get_attribute('id')
+        job_details_dictionary["Title"] = job_listing.find_element(by=By.XPATH, value=".//h2").text
+        job_details_dictionary["Company_Name"] = job_listing.find_element(by=By.XPATH, value=".//div/span[@class='companyName']").text
+        job_details_dictionary["Company_Location"] = job_listing.find_element(by=By.XPATH, value=".//div[@class='companyLocation']").text
+        try:
+            job_details_dictionary["Salary"] = job_listing.find_element(by=By.XPATH, value= ".//div[@class='metadata salary-snippet-container']").get_attribute('textContent')
+        except:
+            pass   
+        list_of_all_jobs_details.append(job_details_dictionary)
+        print(list_of_all_jobs_details)
+    return list_of_all_jobs_details
+extract()
+get_job_details()
+print(get_job_details)
+# extract(page='https://uk.indeed.com/jobs?q=Data%20Engineer%20or%20Data%20Scientist%20&l=London%2C%20Greater%20London&start=0')
 # get_job_details()
 # get_job_details()
 # scrape(self=job_indeed)
